ad5fbce379956729f733a3d3e77a030d81919548
dev <dev@example.com>
2026-05-17 05:38:38 +0800

    build: rebuild

--- WeChatPlugin/Modules/MessageTime/MessageTimeHook.m
+++ WeChatPlugin/Modules/MessageTime/MessageTimeHook.m
@@ -359,9 +359,9 @@ static CGRect computeLabelFrame(CGRect cellFrame, CGSize labelSize, NSInteger po
             break;
         case 3:
             if (isSender) {
-                labelFrame.origin.x = cvRight - w * 0.5 - 10;
+                labelFrame.origin.x = cvRight - w * 0.5 - 14;
             } else {
-                labelFrame.origin.x = cvLeft + w * 0.5 - 10;
+                labelFrame.origin.x = cvLeft + w * 0.5 - 14;
             }
             labelFrame.origin.y = cvBottom + h * 0.5;
             break;
