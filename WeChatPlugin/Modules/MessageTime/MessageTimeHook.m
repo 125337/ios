7259727cdd96e8f7852a179d6ae0f1ea8fad033e
dev <dev@example.com>
2026-05-17 21:07:37 +0800

    更新

--- WeChatPlugin/Modules/MessageTime/MessageTimeHook.m
+++ WeChatPlugin/Modules/MessageTime/MessageTimeHook.m
@@ -456,12 +456,12 @@ static CGRect computeLabelFrame(CGRect cellFrame, CGSize labelSize, NSInteger po
             }
             labelFrame.origin.y = cvBottom - h * kStraddleFactor;
             break;
-        case 2: // 消息旁边(远离头像)：气泡外侧 straddle，垂直居中
-        default:
+        case 7: // 消息旁边(=气泡外)：同位置2，气泡外侧 straddle，左偏5pt
+        case 2: // 消息旁边(远离头像)：气泡外侧 straddle，垂直居中，左偏5pt
             if (isSender) {
-                labelFrame.origin.x = cvLeft - w * kStraddleFactor;
+                labelFrame.origin.x = cvLeft - w * kStraddleFactor - 5;
             } else {
-                labelFrame.origin.x = cvRight + w * kStraddleFactor;
+                labelFrame.origin.x = cvRight + w * kStraddleFactor + 5;
             }
             labelFrame.origin.y = cvBottom - h * kStraddleFactor;
             break;
