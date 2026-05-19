68c4f84b6b77370d0a81acf156263ce61aa948ee
dev <dev@example.com>
2026-05-19 19:41:11 +0800

    build: rebuild

--- WeChatPlugin/Config/PluginConfig.m
+++ WeChatPlugin/Config/PluginConfig.m
@@ -326,11 +326,14 @@ static void configLog(NSString *content) {
     if (!hex.length) return nil;
     NSString *clean = [hex stringByReplacingOccurrencesOfString:@"#" withString:@""];
     if (clean.length < 6) return nil;
-    unsigned int r = 0, g = 0, b = 0;
+    unsigned int r = 0, g = 0, b = 0, a = 255;
     [[NSScanner scannerWithString:[clean substringWithRange:NSMakeRange(0, 2)]] scanHexInt:&r];
     [[NSScanner scannerWithString:[clean substringWithRange:NSMakeRange(2, 2)]] scanHexInt:&g];
     [[NSScanner scannerWithString:[clean substringWithRange:NSMakeRange(4, 2)]] scanHexInt:&b];
-    return [UIColor colorWithRed:r / 255.0 green:g / 255.0 blue:b / 255.0 alpha:1.0];
+    if (clean.length >= 8) {
+        [[NSScanner scannerWithString:[clean substringWithRange:NSMakeRange(6, 2)]] scanHexInt:&a];
+    }
+    return [UIColor colorWithRed:r / 255.0 green:g / 255.0 blue:b / 255.0 alpha:a / 255.0];
 }
 
 - (BOOL)isDarkMode {
