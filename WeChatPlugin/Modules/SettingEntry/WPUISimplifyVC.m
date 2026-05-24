41b6e9bddf92ff5cee2aaf4ca1cbd1c5f1fe431d
dev <dev@example.com>
2026-05-24 12:47:20 +0800

    build: 20260524_124720

--- WeChatPlugin/Modules/SettingEntry/WPUISimplifyVC.m
+++ WeChatPlugin/Modules/SettingEntry/WPUISimplifyVC.m
@@ -318,17 +318,23 @@ static void WPUISimplifyViewDidLoad(id self, SEL _cmd) {
     if (!subClass) {
         subClass = objc_allocateClassPair(WPGetBaseClass(), "WPUISimplifyVC", 0);
         if (subClass) {
-            class_addMethod(subClass, NSSelectorFromString(@"viewDidLoad"), (IMP)WPUISimplifyViewDidLoad, "v@:");
             objc_registerClassPair(subClass);
             WPLog(@"UI", @"[Sub] WPUISimplifyVC class created");
         } else {
             WPLog(@"UI", @"[Sub] WPUISimplifyVC class create FAILED");
+            return nil;
         }
     }
-    if (subClass) {
-        return [[subClass alloc] init];
+    // 始终更新 IMP，确保新旧代码都能覆盖
+    Method m = class_getInstanceMethod(subClass, NSSelectorFromString(@"viewDidLoad"));
+    if (m) {
+        method_setImplementation(m, (IMP)WPUISimplifyViewDidLoad);
+        WPLog(@"UI", @"[Sub] WPUISimplifyVC viewDidLoad IMP updated");
+    } else {
+        class_addMethod(subClass, NSSelectorFromString(@"viewDidLoad"), (IMP)WPUISimplifyViewDidLoad, "v@:");
+        WPLog(@"UI", @"[Sub] WPUISimplifyVC viewDidLoad IMP added");
     }
-    return nil;
+    return [[subClass alloc] init];
 }
 
 @end
