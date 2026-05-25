f5a726fff74ffa7fc2626a6e50b21ea0ac52b90b
dev <dev@example.com>
2026-05-26 02:21:07 +0800

    build: 20260526_022107 - align attachment layout UI with simplify pattern

--- WeChatPlugin/Modules/SettingEntry/WPUIAttachmentLayoutVC.m
+++ WeChatPlugin/Modules/SettingEntry/WPUIAttachmentLayoutVC.m
@@ -4,6 +4,23 @@
 static NSString *const kAttachLayoutEnabledKey = @"AttachLayoutEnabled";
 
 /// ========== buildUI ==========
+static void WPUIAttachLayoutBuildUI(id self, SEL _cmd);
+
+#pragma mark - ========== viewDidLoad ==========
+
+static void WPUIAttachLayoutViewDidLoad(id self, SEL _cmd) {
+    Class uiVC = objc_getClass("UIViewController");
+    Method m = class_getInstanceMethod(uiVC, _cmd);
+    if (m) ((void (*)(id, SEL))method_getImplementation(m))(self, _cmd);
+
+    UIViewController *vc = (UIViewController *)self;
+    vc.title = @"附件布局优化";
+
+    WPUIAttachLayoutBuildUI(self, _cmd);
+}
+
+#pragma mark - ========== buildUI ==========
+
 static void WPUIAttachLayoutBuildUI(id self, SEL _cmd) {
     UIViewController *vc = (UIViewController *)self;
     CGFloat w = vc.view.bounds.size.width;
@@ -12,7 +29,7 @@ static void WPUIAttachLayoutBuildUI(id self, SEL _cmd) {
     NSUserDefaults *d = [NSUserDefaults standardUserDefaults];
     BOOL enabled = [d boolForKey:kAttachLayoutEnabledKey];
 
-    // 清除旧的 scrollView
+    // 清除旧的 scrollView，重新创建
     UIView *oldSV = objc_getAssociatedObject(self, "buildUISV");
     [oldSV removeFromSuperview];
 
@@ -38,24 +55,35 @@ static void WPUIAttachLayoutBuildUI(id self, SEL _cmd) {
     UISwitch *sw = [[UISwitch alloc] init];
     sw.on = enabled;
     sw.onTintColor = WPSwOn();
-    sw.frame = CGRectMake(w - kPad * 2 - 51, scy + 6.5, 51, 31);
+    sw.frame = CGRectMake(w - kPad * 3 - 51, scy + 6.5, 51, 31);
     [sw addTarget:(id)self action:@selector(onAttachLayoutSwitch:) forControlEvents:UIControlEventValueChanged];
     [switchCard addSubview:sw];
     scy += kRowH;
 
     CGRect scf = switchCard.frame; scf.size.height = scy; switchCard.frame = scf;
     [sv addSubview:switchCard];
-    y += scy + 16;
+    y += scy + 8;
+
+    // 关闭状态：只显示开关卡片
+    if (!enabled) {
+        UILabel *footer = [[UILabel alloc] initWithFrame:CGRectMake(kPad, y, w - kPad * 2, 40)];
+        footer.text = @"修改后将在下次启动时生效";
+        footer.font = [UIFont systemFontOfSize:12];
+        footer.textColor = WPT3();
+        footer.textAlignment = NSTextAlignmentCenter;
+        [sv addSubview:footer];
+        y += 48;
+        sv.contentSize = CGSizeMake(w, y);
+        WPLog(@"UI", @"[Sub] WPUIAttachLayoutBuildUI done (enabled=0, switch only)");
+        return;
+    }
 
     // ========== 子功能：布局设置 ==========
     UILabel *secHeader = WPMakeSectionHeader(@"布局设置", y, w);
     [sv addSubview:secHeader];
-    NSMutableArray *expandViews = [NSMutableArray array];
-    [expandViews addObject:secHeader];
     y += 32;
 
     UIView *layoutCard = WPMakeCard(y, w);
-    [expandViews addObject:layoutCard];
     CGFloat lcy = 0;
 
     NSString *colsKey = @"AttachLayout_Columns";
@@ -67,6 +95,7 @@ static void WPUIAttachLayoutBuildUI(id self, SEL _cmd) {
     {
         UIButton *row = WPAddEditableRowWithArrow(layoutCard, lcy, w, @"每行列数", colsVal, handler);
         objc_setAssociatedObject(row, "editNSKey", colsKey, OBJC_ASSOCIATION_COPY_NONATOMIC);
+        objc_setAssociatedObject(row, "editDefault", @"", OBJC_ASSOCIATION_COPY_NONATOMIC);
         lcy += kRowH;
     }
     // 显示行数
@@ -75,18 +104,13 @@ static void WPUIAttachLayoutBuildUI(id self, SEL _cmd) {
         lcy = round((lcy + 1.0 / scale) * scale) / scale;
         UIButton *row = WPAddEditableRowWithArrow(layoutCard, lcy, w, @"显示行数", rowsVal, handler);
         objc_setAssociatedObject(row, "editNSKey", rowsKey, OBJC_ASSOCIATION_COPY_NONATOMIC);
+        objc_setAssociatedObject(row, "editDefault", @"", OBJC_ASSOCIATION_COPY_NONATOMIC);
         lcy += kRowH;
     }
 
     CGRect lcf = layoutCard.frame; lcf.size.height = lcy; layoutCard.frame = lcf;
     [sv addSubview:layoutCard];
-    y += lcy + 16;
-
-    // 绑定 expandViews + 设置初始 hidden
-    objc_setAssociatedObject(sw, "expandViews", expandViews, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    for (UIView *v in expandViews) {
-        v.hidden = !enabled;
-    }
+    y += lcy + 8;
 
     // 提示文字
     UILabel *footer = [[UILabel alloc] initWithFrame:CGRectMake(kPad, y, w - kPad * 2, 40)];
@@ -107,27 +131,9 @@ static void WPUIAttachLayoutBuildUI(id self, SEL _cmd) {
 static void onAttachLayoutSwitchIMP(id self, SEL _cmd, UISwitch *sender) {
     [[NSUserDefaults standardUserDefaults] setBool:sender.on forKey:kAttachLayoutEnabledKey];
     [[NSUserDefaults standardUserDefaults] synchronize];
-    WPLog(@"UI", @"[AttachLayout] Toggle=%d", sender.on);
+    WPLog(@"UI", @"[AttachLayout] Toggle=%d, rebuilding UI", sender.on);
 
-    NSArray *expandViews = objc_getAssociatedObject(sender, "expandViews");
-    for (UIView *v in expandViews) {
-        v.hidden = !sender.on;
-    }
-    // 标记 rebuild（下次 viewDidLoad 完全重建）
-    objc_setAssociatedObject(sender, "needsRebuild", @YES, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
-
-#pragma mark - ========== viewDidLoad ==========
-
-static void WPUIAttachLayoutViewDidLoad(id self, SEL _cmd) {
-    Class uiVC = objc_getClass("UIViewController");
-    Method m = class_getInstanceMethod(uiVC, _cmd);
-    if (m) ((void (*)(id, SEL))method_getImplementation(m))(self, _cmd);
-
-    UIViewController *vc = (UIViewController *)self;
-    vc.title = @"附件布局优化";
-
+    // 与界面简化一致：开关切换时调用 buildUI 完全重建
     WPUIAttachLayoutBuildUI(self, _cmd);
 }
 
